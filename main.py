e829b60b01baafabbb121040f02d795ad4d39548
Manoj <Manoj.Das@mu-sigma.com>
2020-07-26 18:52:51 +0530

    Integrated all the Feature Repo segment of code and modelFinder function clean up and fix done, included spark initiation from properties file

--- main.py
+++ main.py
@@ -15,13 +15,16 @@
 '''
 
 # Library imports
+import re
 import os
 import ast
 import glob
 import argparse
 import configparser
-from pyspark.sql import sparkSession
-from bricks.sparkBrick import getSparkFrameFromCSV, mojoModelScoring, pmmlModelScoring, pojoModelScoring, pickleModelScoring
+import pandas as pd
+from functools import reduce
+from pyspark.sql import SparkSession
+# from bricks.sparkBrick import getSparkFrameFromCSV, mojoModelScoring, pmmlModelScoring, pojoModelScoring, pickleModelScoring
 
 ######### Operational Functions
 myTitle = lambda x: "my" + x[0].title() + x[1:]
@@ -75,7 +78,7 @@ def modelFinder(modelPath, supportedFormats):
         message (str)         : message explaining the status of function execution 
         modelFile (str)       : name of model file found in the directory
     """
-    if not os.pathisdir(modelPath):
+    if not os.path.isdir(modelPath):
         message = "Error! Specified mdoel path does not exist: " + modelPath
         print("\n" + message + "\n")
         return False, message, None, None
@@ -121,27 +124,74 @@ def tryExcept(x, y):
 
 if __name__ == "__main__":
     # Argument parsing using config file if not specified and final check of arguments
+    try:
+        inAbsoluteCodePath = os.path.split(os.path
+            .realpath(__file__))[0]
+    except Exception as e:
+        inAbsoluteCodePath = os.path.realpath(".")
+
     inConfigFileName = "test_config.cfg"
+    inPipelineArgsFileName = "pipelineArguments.csv"
+    inQueryFileName = "fr_query.csv"
+    inConfigFile = os.path.join(inAbsoluteCodePath, inConfigFileName)
+    inPipelineArgsFile = os.path.join(inAbsoluteCodePath, inPipelineArgsFileName)
+    inQueryFile = os.path.join(inAbsoluteCodePath, inQueryFileName)
+    
     #change the below dictionary accordingly, update the config file accordingly
     inArgsDiction = \
         {
             "mainAttributes": [["appName", "str"], ["datasetName", "str"]],
             "scoringAttributes": [["columnSelection", "list"], ["columnOut", "list"]]
         }
+    # inPipelineArgs = set(map(lambda x: x.strip().lower(), open(inPipelineArgsFile).readlines()))
+    inPipelineArgs = pd.read_csv(inPipelineArgsFile, header=None, names=["arg", "value"]).iloc[:, :2]
+    inPipelineArgs["arg"] = inPipelineArgs["arg"].apply(lambda x: x.strip().lower())
+    inPipelineArgs = inPipelineArgs.set_index("arg")
+    checkAndTerminate(len(inPipelineArgs.index) == len(set(inPipelineArgs.index)), 
+                inPipelineArgsFile + " contains duplicates: " + \
+                ", ".join((lambda y: set([x for x in y if y.count(x) > 1]))(list(inPipelineArgs.index))))
+    inQueryFrame = pd.read_csv(inQueryFile, header=None).iloc[:, 0]
+    if set(inQueryFrame.apply(lambda x:  re.findall(r"(\b(?:[a-z]+)\b(?:\s+(?:[A-Z]+[a-z]?[A-Z]*|[A-Z]*[a-z]?[A-Z]+)\b)*)", x)).sum()) - set(inPipelineArgs.index):
+        checkAndTerminate(False, "\n" + \
+            ", ".join(set(inQueryFrame.apply(lambda x:  re.findall(r"(\b(?:[a-z]+)\b(?:\s+(?:[A-Z]+[a-z]?[A-Z]*|[A-Z]*[a-z]?[A-Z]+)\b)*)", x)).sum()) - \
+            set(inPipelineArgs.index)) + " are not present in the pipeline arguments file: " + inPipelineArgsFile + "\n")
+    if set(inPipelineArgs.index) - set(inQueryFrame.apply(lambda x:  re.findall(r"(\b(?:[a-z]+)\b(?:\s+(?:[A-Z]+[a-z]?[A-Z]*|[A-Z]*[a-z]?[A-Z]+)\b)*)", x)).sum()):
+        print("Warning: " + \
+            ", ".join(set(inPipelineArgs.index) - set(inQueryFrame.apply(lambda x:  re.findall(r"(\b(?:[a-z]+)\b(?:\s+(?:[A-Z]+[a-z]?[A-Z]*|[A-Z]*[a-z]?[A-Z]+)\b)*)", x)).sum())) + \
+            " are not used in query file: " + inQueryFileName)
     # Main Arguments handling
-    try:
-        inAbsoluteCodePath = os.path.split(os.path.realpath(__file__))[0]
-    except Exception as e:
-        inAbsoluteCodePath = os.path.realpath(".")
-    inConfigFile = os.path.join(inAbsoluteCodePath, inConfigFileName)
     dataTypeDecode = {"str": "str", "int": "int", "bool": "bool", "list": "str", "float": "float"}
     inParser = argparse.ArgumentParser()
+    list(map(lambda y: eval("inParser.add_argument('--" + y + "', type=str" + \
+                                                                        ", required=True, help='specify " + y + "')"),
+            inPipelineArgs[inPipelineArgs.value.isnull()].index)) is None
+    list(map(lambda y: eval("inParser.add_argument('--" + y + "', type=str" + \
+                                                                        ", help='specify " + y + "')"),
+            inPipelineArgs[inPipelineArgs.value.notnull()].index)) is None
     list(map(lambda x: list(
         map(lambda y: eval("inParser.add_argument('--" + y[0] + "', type=" + dataTypeDecode[y[1]] + \
                                                                         ", help='specify " + y[0] + "')"),
             inArgsDiction[x])), inArgsDiction.keys())) is None
     inParseArgs = inParser.parse_args()
-    if os.path.isfile(inConfigFile):
+    inParameterTypeParsersArg = {"str": lambda x, y: x,
+                             "bool": lambda x, y: x,
+                             "int": lambda x, y: x,
+                             "float": lambda x, y: x,
+                             "list": lambda x, y: tryExcept(lambda: generateList(x.strip(), y), 
+                                                           lambda: checkAndTerminate(False, 
+                                                            "Argument "+ y + " not correctly parsed"))}
+    inGetConfig = lambda p, r: (lambda x: None if x is None else inParameterTypeParsersArg[p](x, r))(eval("inParseArgs." + r))
+    list(map(lambda y: createGlobalObject(y, inGetConfig("str", y)), inPipelineArgs[inPipelineArgs.value.isnull()].index)) is None
+    list(map(lambda y: createGlobalObject(y, (lambda m, n: m if n is None else n)(str(inPipelineArgs.loc[y]["value"]), inGetConfig("str", y))), 
+        inPipelineArgs[inPipelineArgs.value.notnull()].index)) is None
+    if not os.path.isfile(inConfigFile):
+        print("Warning: Config file not present")
+        list(map(lambda x: list(map(lambda y: createGlobalObject(myTitle(y[0]), inGetConfig(y[1], y[0])), inArgsDiction[x])),
+                inArgsDiction.keys())) is None
+        list(map(lambda x: list(
+            map(lambda y: checkAndTerminate(eval(myTitle(y[0])) is not None, y[0] + " is not specified"), inArgsDiction[x])),
+                inArgsDiction.keys())) is None
+    else:
         inConfig = configparser.ConfigParser()
         inConfig.read(inConfigFile)
         inParameterTypeParsersConf = {"str": lambda x, y: (lambda x: None if x == '' else x)(inConfig.get(x, y).strip()),
@@ -159,38 +209,35 @@ if __name__ == "__main__":
                                                             (inConfig.get(x, y).strip()), y),
                                                             lambda: checkAndTerminate(False, 
                                                                 "Argument "+ y + " not correctly placed in config file"))}
-        inParameterTypeParsersArg = {"str": lambda x, y: x,
-                                     "bool": lambda x, y: x,
-                                     "int": lambda x, y: x,
-                                     "float": lambda x, y: x,
-                                     "list": lambda x, y: tryExcept(lambda: generateList(x.strip(), y), 
-                                                                   lambda: checkAndTerminate(False, 
-                                                                    "Argument "+ y + " not correctly parsed"))}
         inGetConfig = lambda p, q, r: (lambda x, y: y if x is None else inParameterTypeParsersArg[p](x, r))(eval("inParseArgs." + r),
                                                                          inParameterTypeParsersConf[p](q, r))
         list(map(lambda x: list(map(lambda y: createGlobalObject(myTitle(y[0]), inGetConfig(y[1], x, y[0])), inArgsDiction[x])),
                 inArgsDiction.keys())) is None
-    else:
-        print("Warning: Config file not present")
-        inGetConfig = lambda p, q, r: eval("inParseArgs." + r)
-        list(map(lambda x: list(map(lambda y: createGlobalObject(myTitle(y[0]), inGetConfig(y[1], x, y[0])), inArgsDiction[x])),
-                inArgsDiction.keys())) is None
-        list(map(lambda x: list(
-            map(lambda y: checkAndTerminate(eval(myTitle(y[0])) is not None, y[0] + " is not specified"), inArgsDiction[x])),
-                inArgsDiction.keys())) is None
-    
-    inModelDiction = {'mojo': mojoModelScoring, 'pojo': pojoModelScoring, 'pickle': pickleModelScoring, 'pmml': pmmlModelScoring}
+
+    inModelDiction = {'mojo': "fiwhfw", 'pojo': "fiwhfw", 'pickle': "fiwhfw", 'pmml': "fiwhfw"}
+        # inModelDiction = {'mojo': mojoModelScoring, 'pojo': pojoModelScoring, 'pickle': pickleModelScoring, 'pmml': pmmlModelScoring}    
 
     # Looking for model file
-    inStatus, inMessage, modelType, modelFile = modelFinder(os.path.join(inAbsoluteCodePath, model), inModelDiction.keys())
+    inStatus, inMessage, modelType, modelFile = modelFinder(os.path.join(inAbsoluteCodePath, "model"), inModelDiction.keys())
     checkAndTerminate(inStatus, inMessage)
-
+    inQueryFrame = inQueryFrame.apply(lambda n: reduce(lambda x, y: x.replace(y[0], y[1]), [n] + \
+                    list(map(lambda x: (x, eval(x)), inPipelineArgs.index))))
+    
     # Initialising spark    
     inSpark = SparkSession.builder.appName(myAppName).getOrCreate()
-    _,_, inScoreFrame = getSparkFrameFromCSV(inSpark, os.path.join(inAbsoluteCodePath, "inputs", myDatasetName))
+    
+    # Running Data preperation queries to get feature data
+    for query in inQueryFrame.iloc[:-1]:
+        print(query)
+        # spark.sql(query)
+    print(inQueryFrame.iloc[-1])
+    # inScoreFrame = spark.sql(inQueryFrame.iloc[-1])
+
+    inStatus, inMessage, inScoreFrame = getSparkFrameFromCSV(inSpark, os.path.join(inAbsoluteCodePath, "inputs", myDatasetName))
+    checkAndTerminate(inStatus, inMessage)
     
     # Calling the appropriate function to score the model according to the model file format found
-    inStatus, inMessage, df = inModelDiction[modelType](inSpark, inScoreFrame, os.path.join(inAbsoluteCodePath, "model", modelFile), 
+    inStatus, inMessage, inOutputFrame = inModelDiction[modelType](inSpark, inScoreFrame, os.path.join(inAbsoluteCodePath, "model", modelFile), 
                                             myColumnSelection, myColumnOut)
     try:
         get_ipython().__class__.__name__ is None
